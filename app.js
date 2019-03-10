7ce2d3bb41e63c226f98611695dd891162a625b7
Mohan Kumar B <mkumarb@localhost.localdomain>
2019-03-10 21:28:50 +0530

    Full logic

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var express = require('express');
-var request = require('request');
+var express     = require('express'),
+    request     = require('request'),
+    bodyParser  = require("body-parser");
 
 app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/static'))
+
 var options = {
     ///https://api.github.com/repos/:owner/:repo/issues
-    url: 'https://api.github.com/repos/toddmotto/public-apis/issues',
+    url: 'https://api.github.com/repos/mohanb95/count-github-issues/issues',
     headers: {
         'User-Agent': 'Github open issues',
         'Content-type': 'application/json',
@@ -49,49 +56,76 @@ var getAllOpenIssues = function(i, optionsNew, pageCount, all_open_issues, retur
     })
 }
 
-app.get('/', function(req,res){
-    var all_open_issues = [];
+var removePullRequestsAndGetIssueCount = function(all_issues,returnOpenIssuesCount){
+    issuesProcessed = 0;
     var open_issues = [];
+    var sevenDaysOldIssues = 0, twentyFourHourOldIssues = 0;
+    all_issues.forEach(issue => {
+        issuesProcessed++;
+        if(!issue.hasOwnProperty('pull_request')){
+            open_issues.push(issue);
+            var issueCreateDate = new Date(issue.created_at);
+            var oneDayAgo = new Date();
+            oneDayAgo.setDate(oneDayAgo.getDate() - 1);
+            var sevenDaysAgo = new Date();
+            sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+            if(issueCreateDate.getTime() > oneDayAgo.getTime()){
+                twentyFourHourOldIssues++;
+            } else if(issueCreateDate.getTime() > sevenDaysAgo.getTime()){
+                sevenDaysOldIssues++;
+            } 
+            if(issuesProcessed === all_issues.length){
+                var issueCount = {
+                    allOpenIssues: open_issues.length,
+                    twentyFourHourOld: twentyFourHourOldIssues,
+                    betweenOneAndSevenDaysOld: sevenDaysOldIssues - twentyFourHourOldIssues,
+                    olderThanSevenDays: open_issues.length - sevenDaysOldIssues
+                }
+                returnOpenIssuesCount(open_issues, issueCount);
+            }
+        } else {
+            if(issuesProcessed === all_issues.length){
+                var issueCount = {
+                    allOpenIssues: open_issues.length,
+                    twentyFourHourOld: twentyFourHourOldIssues,
+                    betweenOneAndSevenDaysOld: sevenDaysOldIssues - twentyFourHourOldIssues,
+                    olderThanSevenDays: open_issues.length - sevenDaysOldIssues
+                }
+                returnOpenIssuesCount(open_issues, issueCount);
+            }
+        }
+    })
+}
+
+app.post('/', function(req,res){
+    var all_open_issues = [];
+    options.url = req.body.repoURL;
     getPageCount(function(pageCount){
         console.log("Page Count: ",pageCount);
         getAllOpenIssues(1, options, pageCount, all_open_issues, function(all_open_issues){
             this.all_open_issues = all_open_issues;
-            res.send(this.all_open_issues);
+            if(this.all_open_issues.length === 0){
+                var issueCount = {
+                    allOpenIssues: 0,
+                    twentyFourHourOld: 0,
+                    betweenOneAndSevenDaysOld: 0,
+                    olderThanSevenDays: 0
+                }
+                console.log(issueCount);
+                res.send(issueCount);
+            }else{
+                removePullRequestsAndGetIssueCount(this.all_open_issues,function(open_issues,issueCount){
+                    console.log(issueCount);
+                    res.send(issueCount);
+                });
+            }
         })
     });
 })
 
-    // var r = request.get(options,function(error,response,body){
-        
-    //     link_urls = r.responseContent.headers.link;
-    //     console.log("Link header urls: ",r.responseContent.headers.link)
-    //     if(!error && response.statusCode == 200){
-    //         all_open_issues = [...JSON.parse(body)];
-    //         console.log("Issue count: ",all_open_issues.length)
-    //         all_open_issues.forEach(issue => {
-    //             // console.log("In for loop")
-    //             issuesProcessed++;
-    //             if(!issue.hasOwnProperty('pull_request')){
-    //                 open_issues.push(issue);
-    //             } 
-    //             if((issuesProcessed === all_open_issues.length) && link_urls===undefined){ //
-    //                 console.log("Total number of open issues: ",open_issues.length);
-    //                 console.log("Issues processed: ", issuesProcessed)
-    //                 return open_issues;
-    //             } else if((issuesProcessed === all_open_issues.length) && link_urls!==undefined){ //
-                    
-    //                 console.log(options.qs)
-    //                 getOpenIssues(options, open_issues)
-    //             }
-    //         });   
-    //     } else {
-    //         console.log("Request failed with error: ", error);
-    //         res.send(error);
-    //     }
-    // })
-
-
-
+app.get('/',function(req,res){
+    res.render('index')
+})
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server has started!!");
