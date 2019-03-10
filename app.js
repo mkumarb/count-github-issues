e9039003fa60e738cf2f8f0c3dc81d6335c1f2c4
Mohan Kumar B <mkumarb@localhost.localdomain>
2019-03-10 19:00:22 +0530

    Get all open issues

--- app.js
+++ app.js
@@ -13,43 +13,85 @@ var options = {
     },
     qs: {
         state: 'open',
-        per_page: 30,
+        per_page: 100,
         page: 1
     }
 };
 
+function getPageCount(returnValue){
+    request.get(options,function(error,response,body){
+        link_urls = response.headers.link
+        if(link_urls === undefined){
+            returnValue(1);
+        }else {
+            link_urls.split(',').forEach((api_link) => {
+                if(api_link.indexOf('rel="last"')>=0)
+                    returnValue(parseInt(api_link[api_link.indexOf('&page=')+6]));
+            })
+        }
+    })
+}
+
+var getAllOpenIssues = function(i, optionsNew, pageCount, all_open_issues, returnOpenIssueList) {
+    optionsNew.qs.page = i;
+    request.get(optionsNew,function(error,response,body){
+        if(!error && response.statusCode == 200){
+            all_open_issues = [...all_open_issues,...JSON.parse(body)];
+            if(i===pageCount){
+                returnOpenIssueList(all_open_issues)
+            }else{
+                getAllOpenIssues(i+1, optionsNew, pageCount, all_open_issues, returnOpenIssueList)
+            }  
+        } else {
+            console.log("Request failed with error: ", error);
+            res.send(error);
+        }
+    })
+}
+
 app.get('/', function(req,res){
     var all_open_issues = [];
     var open_issues = [];
-    var issuesProcessed = 0;
-    var r = request.get(options,function(error,response,body){
-        if(r.responseContent.headers.link){
-            console.log("We have a next page!");
-            console.log(r.responseContent.headers.link);
-        }else{
-            console.log("We do not have a next page!");
-        }
-        all_open_issues = JSON.parse(body);
-        res.send(all_open_issues);
-        // if(!error && response.statusCode == 200){
-        //     all_open_issues = JSON.parse(body);
-        //     all_open_issues.forEach(issue => {
-        //         issuesProcessed++;
-        //         if(!issue.hasOwnProperty('pull_request')){
-        //             open_issues.push(issue);
-        //         } 
-        //         if(issuesProcessed === all_open_issues.length){
-        //             console.log("Total number of open issues: ",open_issues.length);
-        //             res.send(r);
-        //         }
-        //     });   
-        // } else {
-        //     console.log("Request failed with error: ", error);
-        //     res.send(r);
-        // }
-        // res.send(body)
-    })
+    getPageCount(function(pageCount){
+        console.log("Page Count: ",pageCount);
+        getAllOpenIssues(1, options, pageCount, all_open_issues, function(all_open_issues){
+            this.all_open_issues = all_open_issues;
+            res.send(this.all_open_issues);
+        })
+    });
 })
+
+    // var r = request.get(options,function(error,response,body){
+        
+    //     link_urls = r.responseContent.headers.link;
+    //     console.log("Link header urls: ",r.responseContent.headers.link)
+    //     if(!error && response.statusCode == 200){
+    //         all_open_issues = [...JSON.parse(body)];
+    //         console.log("Issue count: ",all_open_issues.length)
+    //         all_open_issues.forEach(issue => {
+    //             // console.log("In for loop")
+    //             issuesProcessed++;
+    //             if(!issue.hasOwnProperty('pull_request')){
+    //                 open_issues.push(issue);
+    //             } 
+    //             if((issuesProcessed === all_open_issues.length) && link_urls===undefined){ //
+    //                 console.log("Total number of open issues: ",open_issues.length);
+    //                 console.log("Issues processed: ", issuesProcessed)
+    //                 return open_issues;
+    //             } else if((issuesProcessed === all_open_issues.length) && link_urls!==undefined){ //
+                    
+    //                 console.log(options.qs)
+    //                 getOpenIssues(options, open_issues)
+    //             }
+    //         });   
+    //     } else {
+    //         console.log("Request failed with error: ", error);
+    //         res.send(error);
+    //     }
+    // })
+
+
+
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server has started!!");
